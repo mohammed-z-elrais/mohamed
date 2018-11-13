eb4656e5ad2212803c6db36904c34d65bb87e6e1
mohammed-z-elrais <45018286+mohammed-z-elrais@users.noreply.github.com>
2018-11-13 23:56:28 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+int main(void)
+{
+ int n = get_int("moh:");
+  // int n = 5;
+  // for (int i = 0 ; i > n; i++)
+  // {
+  //  printf("m");
+  //  }
+}
